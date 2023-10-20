093aceea8b3b54ed5d09f96632fde94e8712e6b6
shn27 <sohanur1525sohan@gmail.com>
2023-10-20 17:33:15 +0600

    C7:test AbstractClassExampleSecond.java which extends AbstractClassExample.java

---
+++ JavaIntro/src/AbstractClassExampleSecond.java
@@ -0,0 +1,5 @@
+public class AbstractClassExampleSecond extends AbstractClassExample  {
+    public void demo() { // the body of the abstract method is provided here
+        System.out.println("This is a abstract class example");
+    }
+}
