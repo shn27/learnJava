1dd385173630a294fffb45e010cca50d1a7b78bd
shn27 <sohanur1525sohan@gmail.com>
2023-10-20 16:24:45 +0600

    C6:test Add Method Overloading and modify Controller.java

---
+++ JavaIntro/src/MethodOverloading.java
@@ -0,0 +1,9 @@
+public class MethodOverloading {
+    void fun( int a, int b ){
+        System.out.println("sum of a+b = "+ a+b);
+    }
+    void fun( int a, int b , int c ){
+        System.out.println("sum of a+b = "+ a+b+c);
+    }
+
+}
