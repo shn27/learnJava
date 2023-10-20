140fb64e1b5e24febe8e81ecee2563447a935bc8
shn27 <sohanur1525sohan@gmail.com>
2023-10-20 12:42:50 +0600

    C2:test Added Controller.java and Math1.java

---
+++ JavaIntro/src/Controller.java
@@ -0,0 +1,6 @@
+public class Controller {
+    void controller( ){
+        Math1 math = new Math1(10 , 20 ) ;
+        math.calculate() ;
+    }
+}
---
+++ JavaIntro/src/Math1.java
@@ -0,0 +1,17 @@
+public class Math1 {
+    private int x , y ;
+
+    public Math1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void calculate(){
+        System.out.println("Maximum = " + Math.max(this.x , this.y));
+        System.out.println("Minimum = " + Math.min(this.x , this.y));
+        System.out.println("sqrt of 49 = "  + Math.sqrt(49));
+        System.out.println("abs of -4.7 = "  + Math.abs(-4.7));
+        System.out.println("Random number"  + Math.random());
+
+    }
+}
