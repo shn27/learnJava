cd614be70aab3bb56c4dd19ed6f23958a22ead58
shn27 <sohanur1525sohan@gmail.com>
2023-10-20 14:49:56 +0600

    C5:test Array.java added

---
+++ JavaIntro/src/Array.java
@@ -0,0 +1,7 @@
+public class Array {
+    void demo( ){
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+        System.out.println(cars.length);
+    }
+}
